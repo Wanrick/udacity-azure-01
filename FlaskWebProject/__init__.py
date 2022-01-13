fb134c4a04660aa9c7761cb22725b57736a56eb3
Wanrick Willemse <wanrickw@gmail.com>
2022-01-13 21:40:57 +0100

    Attempt at fix logging

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -2,19 +2,21 @@
 The flask application package.
 """
 import logging
-from flask import Flask
+from flask import Flask, session
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
+from flask.logging import default_handler
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.logger.removeHandler(default_handler)
 app.logger.setLevel(logging.WARNING)
-
 streamHandler = logging.StreamHandler()
 streamHandler.setLevel(logging.WARNING)
 app.logger.addHandler(streamHandler)
+app.logger.warning('A new log has dawned!')
 
 Session(app)
 db = SQLAlchemy(app)
